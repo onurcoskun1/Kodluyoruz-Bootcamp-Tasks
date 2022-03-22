15fdfdebdea574e0b286d65aa65f5f0deefbe596
onurcoskun1 <coskunonur01@gmail.com>
2022-03-22 22:41:44 +0300

    odevler

---
+++ Week-2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
